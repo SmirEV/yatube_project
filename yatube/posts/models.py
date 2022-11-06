4e35f41f95e7f56d5cf59b606b73d9943dc7015c
SmirEV <ev_smir@mail.ru>
2022-11-06 22:47:36 +0300

    Before first project review

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -6,7 +6,7 @@ User = get_user_model()
 
 class Group(models.Model):
     title = models.TextField()
-    slug = models.TextField()
+    slug = models.SlugField()
     descrption = models.TextField()
 
     def __str__(self):
--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -4,9 +4,10 @@ from django.urls import path
 from . import views
 
 app_name = 'posts'
+#namespace='posts'
 
 urlpatterns = [
-    path('', views.index, name='main'),
+    path('', views.index, name='index'),
     path('group/', views.group_list),
     #path('group/<slug>/', views.group_posts)
     path('group/<slug:slug>/', views.group_posts, name='group_list')
