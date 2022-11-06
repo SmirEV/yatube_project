12ca0aeba887cacc268a2c1e3364c62e1059d9a9
SmirEV <ev_smir@mail.ru>
2022-11-06 15:45:10 +0300

    Before theme 5/7 lesson 6/6

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,14 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.shortcuts import render
+# Импортируем модель, чтобы обратиться к ней
+from .models import Post
 
 
 def index(request):
-    template = 'posts/index.html'
+    # Одна строка вместо тысячи слов на SQL:
+    # в переменную posts будет сохранена выборка из 10 объектов модели Post,
+    # отсортированных по полю pub_date по убыванию
+    # (от больших значений к меньшим)
+    posts = Post.objects.order_by('-pub_date')[:10]
+    # В словаре context отправляем информацию в шаблон
     context = {
-        'text': 'Это главная страница проекта Yatube'
+        'posts': posts,
     }
-    return render(request, template, context)
+    return render(request, 'posts/index.html', context)
 
 
 def group_list(request):
